4a1c3457dc79e6be31d8c359f35c9c152545cf30
Aomsqian <59518608+Aomsqian@users.noreply.github.com>
2021-08-31 12:39:12 +0800

    Update jd_crazy_joy.js

--- jd_crazy_joy.js
+++ jd_crazy_joy.js
@@ -9,7 +9,7 @@ crazyJoy任务
 ============Quantumultx===============
 [task_local]
 #crazyJoy任务
-10 9 * * * jd_crazy_joy.js, tag=crazyJoy任务, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jd_crazy_joy.png, enabled=true
+#10 9 * * * jd_crazy_joy.js, tag=crazyJoy任务, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jd_crazy_joy.png, enabled=true
 
 ================Loon==============
 [Script]
