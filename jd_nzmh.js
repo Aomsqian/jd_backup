3681e11fcfdb3e71464a176f713ef720c8aa5e65
Aomsqian <59518608+Aomsqian@users.noreply.github.com>
2021-09-02 18:19:06 +0800

    Update jd_nzmh.js

--- jd_nzmh.js
+++ jd_nzmh.js
@@ -9,14 +9,11 @@
 [task_local]
 #女装盲盒
 35 1,23 * * * https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_nzmh.js, tag=女装盲盒, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
-
 ================Loon==============
 [Script]
 cron "5 1,20 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_nzmh.js,tag=女装盲盒
-
 ===============Surge=================
 女装盲盒 = type=cron,cronexp="35 1,23 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_nzmh.js
-
 ============小火箭=========
 女装盲盒 = type=cron,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_nzmh.js, cronexpr="35 1,23 * * *", timeout=3600, enable=true
  */
