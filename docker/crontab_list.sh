85d9acbae3505ad77adffc97f9b9cca2d7ec1be9
Aomsqian <59518608+Aomsqian@users.noreply.github.com>
2021-10-22 21:10:31 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -242,4 +242,5 @@
 23 8,19 * * * node /scripts/jd_selectionOfficer.js >> /scripts/logs/jd_selectionOfficer.log 2>&1
 # 预售福利机
 5 0,2 * * * node /scripts/jd_ys.js >> /scripts/logs/jd_ys.log 2>&1
-
+# hyj
+0 * * * * node /scripts/jd_hyj.js >> /scripts/logs/jd_hyj.log 2>&1
