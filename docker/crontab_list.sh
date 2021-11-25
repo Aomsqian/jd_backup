d90f04d50a5414ea7c10ce07cab42e43504a9f17
Aomsqian <59518608+Aomsqian@users.noreply.github.com>
2021-11-25 17:17:24 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -221,5 +221,4 @@
 31 2,8 * * * node /scripts/jd_mf.js >> /scripts/logs/jd_mf.log 2>&1
 # 牛牛福利
 1 9,19 * * * node /scripts/jd_nnfl.js >> /scripts/logs/jd_nnfl.log 2>&1
-# 热血心跳,狂解压
-10 2,9,17 * * * node /scripts/jd_vivo.js >> /scripts/logs/jd_vivo.log 2>&1
+
